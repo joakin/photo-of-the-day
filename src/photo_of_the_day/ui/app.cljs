8854e96dea1f5ed5f52eb427f17b43acf7547822
joakin <joaquin@chimeces.com>
2015-09-02 00:10:52 +0200

    Initial render of the list of current month's pics.

--- src/photo_of_the_day/ui/app.cljs
+++ src/photo_of_the_day/ui/app.cljs
@@ -1,4 +1,11 @@
 (ns photo-of-the-day.ui.app)
 
-(defn app []
-  [:h1 "Photo of the day"])
+(defn app [state]
+  (let [{:keys [current-month]} @state]
+    [:div
+     [:h1 "Photo of the day"]
+     (for [img (filter #(= (:type %) :bitmap) current-month)]
+       [:div {:key (:name img)}
+        [:h4 (:name img)]
+        [:img {:src (get-in img [:thumbnail :src])}]
+        [:p (:description img)]])]))
