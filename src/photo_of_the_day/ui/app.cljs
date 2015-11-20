01ddb3857f9eb8ffed7db8ea5171884dc4588183
joakin <joaquin@chimeces.com>
2015-11-20 13:31:48 +0100

    Add loader gif while loading data

--- src/photo_of_the_day/ui/app.cljs
+++ src/photo_of_the_day/ui/app.cljs
@@ -1,7 +1,11 @@
 (ns photo-of-the-day.ui.app)
 
-(defn separator []
-  [:div.Separator "&"])
+(defn loader []
+  [:div.sk-cube-grid.Loader
+   (for [i (range 1 10)]
+    [:div {:className (str "sk-cube sk-cube" i)}])
+    ])
+
 
 (defn media [{:keys [obj]}]
   [:a.Media {:href (:url obj) :target "_blank"}
@@ -17,7 +21,9 @@
      [:h1.App-title "Wikipedia Commons"]
      [:h4.App-subtitle "Photos of the month"]
      [:div.App-photos
-      (for [img current-month #_(filter #(= (:type %) :bitmap) current-month)]
-        [:div {:key (:name img)}
-         [media {:obj img}]
-         ])]]))
+      (if-not false
+        [loader]
+        (for [img current-month #_(filter #(= (:type %) :bitmap) current-month)]
+          [:div {:key (:name img)}
+           [media {:obj img}]
+           ]))]]))
