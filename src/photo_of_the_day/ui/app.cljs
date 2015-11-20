0a36804e9c6199a09e90d1b657e58ba887175383
joakin <joaquin@chimeces.com>
2015-11-20 13:15:35 +0100

    Extract components and add classes for styles

--- src/photo_of_the_day/ui/app.cljs
+++ src/photo_of_the_day/ui/app.cljs
@@ -1,11 +1,23 @@
 (ns photo-of-the-day.ui.app)
 
+(defn separator []
+  [:div.Separator "&"])
+
+(defn media [{:keys [obj]}]
+  [:a.Media {:href (:url obj) :target "_blank"}
+   [:div.Media-image [:img {:src (get-in obj [:thumbnail :src])}]]
+   [:div.Media-data
+    [:p.Media-description (:description obj)]
+    ]])
+
 (defn app [state]
+  (.log js/console (clj->js @state))
   (let [{:keys [current-month]} @state]
-    [:div
-     [:h1 "Photo of the day"]
-     (for [img (filter #(= (:type %) :bitmap) current-month)]
-       [:div {:key (:name img)}
-        [:h4 (:name img)]
-        [:img {:src (get-in img [:thumbnail :src])}]
-        [:p (:description img)]])]))
+    [:div.App
+     [:h1.App-title "Wikipedia Commons"]
+     [:h4.App-subtitle "Photos of the month"]
+     [:div.App-photos
+      (for [img current-month #_(filter #(= (:type %) :bitmap) current-month)]
+        [:div {:key (:name img)}
+         [media {:obj img}]
+         ])]]))
